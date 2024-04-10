3c4d8f621c5375bd9ff1bd420c584da07801dfc8
EtiBarr <etienne.barriere.1@ens.etsmtl.ca>
2024-04-10 14:49:18 -0400

    initialization and fetching with db

--- app/src/main/java/com/example/mastermind/dao/bdSQLite.java
+++ app/src/main/java/com/example/mastermind/dao/bdSQLite.java
@@ -1,9 +1,16 @@
 package com.example.mastermind.dao;
 
+import android.annotation.SuppressLint;
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class bdSQLite extends SQLiteOpenHelper {
 
@@ -18,9 +25,8 @@ public class bdSQLite extends SQLiteOpenHelper {
     private static final String COLONNE_RESULTAT = "resultat";
     private static final String COLONNE_NOMBRE_DE_TENTATIVES = "nombre_de_tentatives";
 
-    public bdSQLite(Context context){
+    public bdSQLite(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version){
         super(context, bdSQLite.DATABASE_NOM, null, bdSQLite.DATABASE_VERSION);
-
     }
     @Override
     public void onCreate(SQLiteDatabase db) {
@@ -61,4 +67,51 @@ public class bdSQLite extends SQLiteOpenHelper {
 
     }
 
+    //retourner la liste des partie
+    @SuppressLint("Range")
+    public ArrayList<HashMap<String, String>> retournerListePartie(){
+
+        SQLiteDatabase db = this.getWritableDatabase();
+        ArrayList<HashMap<String, String>> partieList = new ArrayList<>();
+
+        String query = "SELECT _id, email, code_secret, nombre_de_couleurs, resultat, nombre_de_tentatives " +
+                        "FROM "+ TABLE_PARTIE;
+        Cursor cursor = db.rawQuery(query,null);
+        while (cursor.moveToNext()){
+            HashMap<String,String> user = new HashMap<>();
+            user.put("id",cursor.getString(cursor.getColumnIndex(COLONNE_ID)));
+            user.put("Email",cursor.getString(cursor.getColumnIndex(COLONNE_EMAIL)));
+            user.put("Code secret",cursor.getString(cursor.getColumnIndex(COLONNE_CODE_SECRET)));
+            user.put("nombre de couleurs",cursor.getString(cursor.getColumnIndex(COLONNE_NOMBRE_DE_COULEURS)));
+            user.put("Resultat",cursor.getString(cursor.getColumnIndex(COLONNE_RESULTAT)));
+            user.put("Nombre de tentatives",cursor.getString(cursor.getColumnIndex(COLONNE_NOMBRE_DE_TENTATIVES)));
+            partieList.add(user);
+        }
+        cursor.close();
+        return  partieList;
+    }
+
+    //retourner liste de partie par email
+    @SuppressLint("Range")
+    public HashMap<String, String> getPartieAvecEmail(String email) {
+        SQLiteDatabase db = this.getReadableDatabase();
+        HashMap<String, String> partieDetails = new HashMap<>();
+
+        String query = "SELECT * " +
+                        "FROM " + TABLE_PARTIE +
+                        " WHERE " + COLONNE_EMAIL;
+        Cursor cursor = db.rawQuery(query, new String[]{email});
+
+        if (cursor.moveToFirst()) {
+            partieDetails.put("id", cursor.getString(cursor.getColumnIndex(COLONNE_ID)));
+            partieDetails.put("Email", cursor.getString(cursor.getColumnIndex(COLONNE_EMAIL)));
+            partieDetails.put("Code secret", cursor.getString(cursor.getColumnIndex(COLONNE_CODE_SECRET)));
+            partieDetails.put("nombre de couleurs", cursor.getString(cursor.getColumnIndex(COLONNE_NOMBRE_DE_COULEURS)));
+            partieDetails.put("Resultat", cursor.getString(cursor.getColumnIndex(COLONNE_RESULTAT)));
+            partieDetails.put("Nombre de tentatives", cursor.getString(cursor.getColumnIndex(COLONNE_NOMBRE_DE_TENTATIVES)));
+        }
+
+        cursor.close();
+        return partieDetails;
+    }
 }
